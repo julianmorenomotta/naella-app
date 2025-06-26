4469f8d63d9163d0a22883f8902891a459030d3f
julian moreno motta <julian@birdsonmars.com>
2025-06-26 14:32:07 +0200

    WIP: sign up form

---
+++ frontend/naella-app/src/app/auth/sign-up/page.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const formSchema = z.object({
+    email: z.string().email(""),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+
